f1ec9c8566a779d12309974a01fc400b35462334
rs-grewal <grewal.rohan@ufl.edu>
2025-11-14 14:38:55 -0500

    my first commit- Grewal

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a+b
 
+def subtract(a, b):
+    return a-b
 
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError()
+    return b/a
+
+def logarithm(a, b): # base, x
+    if b <= 0 or b == 1:
+        raise ValueError()
+    elif a <= 0:
+        raise ValueError()
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a**b
